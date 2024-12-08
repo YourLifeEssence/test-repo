e5e3a75c9af32413c028971389a0d92ebaf22620
YourLifeEssence <tuti.razuvaev05@mail.ru>
2024-12-09 02:43:32 +0300

    Add candle.* files and fix errors in implementation

--- candle.cpp
+++ candle.cpp
@@ -1,40 +1,29 @@
 ﻿#include "candle.h"
 
+#include <cmath>
 #include <string>
 #include <stdexcept>
 
 Candle::Candle(Price _open, Price _high, Price _low, Price _close)
-	: open(_open)
-	, high(_high)
-	, low(_low)
-	, close(_close)
+	: open(_open), high(_high), low(_low), close(_close)
 {
 }
 
-bool Candle::body_contains(const Price &price) const noexcept
+bool Candle::body_contains(const Price& price) const noexcept
 {
-	if(is_green())
-	{
-		const Price &max = close;
-		const Price &min = open;
-		return price >= min && price <= max;
-	}
-	else
-	{
-		const Price &max = open;
-		const Price &min = close;
-		return price >= min && price <= max;
-	}
+	Price min = std::min(open, close);
+	Price max = std::max(open, close);
+	return price >= min && price <= max;
 }
 
-bool Candle::contains(const Price &price) const noexcept
+bool Candle::contains(const Price& price) const noexcept
 {
 	return price >= low && price <= high;
 }
 
 double Candle::full_size() const noexcept
 {
-	return std::abs(low - high);
+	return std::abs(high - low);
 }
 
 double Candle::body_size() const noexcept
